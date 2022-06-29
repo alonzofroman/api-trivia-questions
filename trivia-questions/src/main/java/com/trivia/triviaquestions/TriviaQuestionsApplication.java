268cf84c8c26b6f75287f1ad03cb7e620d721219
alonzofroman <alonzofroman@gmail.com>
2022-06-28 21:28:42 -0700

    Added spring starter folder

---
+++ trivia-questions/src/main/java/com/trivia/triviaquestions/TriviaQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.trivia.triviaquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaQuestionsApplication.class, args);
+	}
+
+}
---
+++ trivia-questions/src/test/java/com/trivia/triviaquestions/TriviaQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trivia.triviaquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
