46eaee3d63d7fdd46cac05bd8f570cb1c1e3ee11
alonzofroman <alonzofroman@gmail.com>
2022-09-05 16:42:14 -0700

    Added join tables on question model to take the correct answers

--- trivia-questions/src/main/java/com/trivia/triviaquestions/model/Question.java
+++ trivia-questions/src/main/java/com/trivia/triviaquestions/model/Question.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="question")
@@ -24,4 +26,59 @@ public class Question implements Serializable {
     @Column(name="genre")
     private String genre;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn
+    private Set<Answer> answers = new HashSet<>();
+
+    public Question(Integer id, String prompt, String genre, Set<Answer> answers) {
+        this.id = id;
+        this.prompt = prompt;
+        this.genre = genre;
+        this.answers = answers;
+    }
+
+    public Question() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Set<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Set<Answer> answers) {
+        this.answers = answers;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", prompt='" + prompt + '\'' +
+                ", genre='" + genre + '\'' +
+                ", answers=" + answers +
+                '}';
+    }
 }
