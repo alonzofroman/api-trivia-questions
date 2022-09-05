52035b18a27da5e881664f2bf1944f819ee9a82d
alonzofroman <alonzofroman@gmail.com>
2022-09-05 16:33:39 -0700

    Added question model

--- trivia-questions/src/main/java/com/trivia/triviaquestions/model/Question.java
+++ trivia-questions/src/main/java/com/trivia/triviaquestions/model/Question.java
@@ -4,17 +4,24 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @Entity
 @Table(name="question")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-public class Question {
+public class Question implements Serializable {
 
     @Id
     @GeneratedValue
-    @Column
+    @Column(name="question_id")
     private Integer id;
 
     @NotNull(message = "The question must have a prompt.")
+    @Column(name="prompt")
     private String prompt;
+
+    @NotNull(message = "The question must have a genre.")
+    @Column(name="genre")
+    private String genre;
+
 }
