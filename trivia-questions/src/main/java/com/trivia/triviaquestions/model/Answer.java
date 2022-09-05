b5365045614600d548757721db0fe4b2560c8738
alonzofroman <alonzofroman@gmail.com>
2022-09-05 16:43:09 -0700

    Added constructors to answer model

--- trivia-questions/src/main/java/com/trivia/triviaquestions/model/Answer.java
+++ trivia-questions/src/main/java/com/trivia/triviaquestions/model/Answer.java
@@ -27,4 +27,56 @@ public class Answer implements Serializable{
     @NotNull(message = "Answer must have a correct or incorrect value.")
     @Column(name="correct")
     private boolean correct;
+
+    public Answer(Integer id, Question question, String message, boolean correct) {
+        this.id = id;
+        this.question = question;
+        this.message = message;
+        this.correct = correct;
+    }
+
+    public Answer() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", question=" + question +
+                ", message='" + message + '\'' +
+                ", correct=" + correct +
+                '}';
+    }
 }
